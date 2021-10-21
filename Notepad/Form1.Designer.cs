449a7798c3d699f3898cca572c1484902170e131
vnechit <vnechit310301@gmail.com>
2021-10-21 18:17:21 +0300

    Preliminary form

--- Notepad/Form1.Designer.cs
+++ Notepad/Form1.Designer.cs
@@ -28,13 +28,224 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Notes = new System.Windows.Forms.TextBox();
+            this.PrintNotes = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Sortbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Notes
+            // 
+            this.Notes.Location = new System.Drawing.Point(13, 85);
+            this.Notes.Multiline = true;
+            this.Notes.Name = "Notes";
+            this.Notes.Size = new System.Drawing.Size(555, 501);
+            this.Notes.TabIndex = 0;
+            // 
+            // PrintNotes
+            // 
+            this.PrintNotes.Location = new System.Drawing.Point(160, 649);
+            this.PrintNotes.Name = "PrintNotes";
+            this.PrintNotes.Size = new System.Drawing.Size(203, 53);
+            this.PrintNotes.TabIndex = 1;
+            this.PrintNotes.Text = "Вывести все заметки";
+            this.PrintNotes.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "ID",
+            "Категория",
+            "Статус"});
+            this.comboBox1.Location = new System.Drawing.Point(601, 790);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(140, 28);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Сортировать";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(791, 790);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 26);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Поле для ввода критерия";
+            // 
+            // Sortbutton
+            // 
+            this.Sortbutton.Location = new System.Drawing.Point(1019, 782);
+            this.Sortbutton.Name = "Sortbutton";
+            this.Sortbutton.Size = new System.Drawing.Size(125, 34);
+            this.Sortbutton.TabIndex = 4;
+            this.Sortbutton.Text = "Сортировать";
+            this.Sortbutton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(600, 727);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Удалить заметку";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(791, 724);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(213, 26);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Удаление по ID";
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(1019, 724);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(125, 35);
+            this.DeleteButton.TabIndex = 7;
+            this.DeleteButton.Text = "Удалить";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(618, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Добавить новую заметку";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(840, 85);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(293, 261);
+            this.textBox3.TabIndex = 9;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(622, 221);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(194, 51);
+            this.AddButton.TabIndex = 10;
+            this.AddButton.Text = "Добавить";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(618, 355);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Изменить заметку";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(840, 355);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(293, 261);
+            this.textBox4.TabIndex = 12;
+            // 
+            // EditButton
+            // 
+            this.EditButton.Location = new System.Drawing.Point(622, 478);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(194, 51);
+            this.EditButton.TabIndex = 13;
+            this.EditButton.Text = "Изменить";
+            this.EditButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(791, 662);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(213, 26);
+            this.textBox5.TabIndex = 14;
+            this.textBox5.Text = "Текст для поиска";
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(1019, 658);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(125, 35);
+            this.SearchButton.TabIndex = 15;
+            this.SearchButton.Text = "Поиск";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(601, 672);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Поиск заметок";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1188, 854);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.EditButton);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Sortbutton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.PrintNotes);
+            this.Controls.Add(this.Notes);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox Notes;
+        private System.Windows.Forms.Button PrintNotes;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Sortbutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button EditButton;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Label label4;
     }
 }
 
